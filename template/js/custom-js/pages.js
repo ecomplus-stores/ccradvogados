8bf3396340ba8e603834ea1ff98e9543b6e3a38b
eduvlemes <edu.vlemes@gmail.com>
2022-08-22 23:48:55 -0300

    ícones, formulário, artigos e profissionais

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -62,4 +62,38 @@ $('.ccr_mobile_menu_trigger').click(function(){
 });
 $('.ccr_mobile_menu_close').click(function(){
     $('#ccr_mobile_menu').removeClass('open');
-})
+});
+
+// $(".anchor-menu a").click(function(e) {
+//     e.preventDefault();
+//     var aid = $(this).attr("href");
+//     $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+// });
+
+$('.apx_form:not(.avise-me)').submit(function(e){
+    e.preventDefault();
+    var mail = [];
+    mail.form = $(this);
+    mail.destination = $(this).find('[name="destination]').val() != undefined ? $(this).find('input[name="destination"]').val() : "edu.vlemes@gmail.com";
+    mail.replyTo = $(this).find('input[name="email"]').val();
+    mail.subject = $(this).find('input[name="subject"]').val();
+    mail.body = "";
+
+    mail.form.find('input:not([type="hidden"]), textarea').each(function(){
+        mail.body = mail.body + $(this).closest('div').find('label').text() + ': ' + $(this).val() + '<br>';
+    });
+
+    axios.post('https://us-central1-marketingtools-ecomplus.cloudfunctions.net/app/alpix/apx_sendmail', {
+        storeId : storefront.settings.store_id,
+        destination : mail.destination,
+        subject : mail.subject,
+        content : mail.body,
+        reply_mail: mail.replyTo
+    })
+    .then(function(response){
+        alert(response.data.msg)
+        if(!response.data.error){
+            mail.form.find('input[type="text"],input[type="email"],textarea,input[type="tel"]').val('')
+        }
+    })    
+});
